413df49def387cec2337a13b353030d53f7fc72e
atifkhan3008 <atif3008@gmail.com>
2018-08-10 00:07:13 +0500

    Added cpp file

---
+++ Facctorial.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+  int k, n, fact = 1;
+ 
+  cout << "Enter a number to calculate its factorial" <<endl;
+  cin >> n;
+ 
+  for (k = 1; k <= n; k++)
+  {
+  fact = fact * k;
+  }
+  cout << "Factorial of " << n <<"="<< fact;
+}
